0b1ef51ca0d38c76043dab4383a4a75148fabb2f
taveirafracenir-cloud <taveirafracenir@gmail.com>
2025-09-25 22:52:13 -0300

    Create kernel.C

---
+++ KERNEL/kernel.C
@@ -0,0 +1,8 @@
+// Arquivo: kernel.c
+void main(void) {
+    while(1) {
+        // O loop infinito do kernel.
+        // O código do seu sistema operacional vai ser adicionado aqui.
+        // Por exemplo, você pode adicionar a lógica para imprimir caracteres na tela.
+    }
+}
